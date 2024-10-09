0dc95e794fadb27a1b77cbc60cc4475b9d560436
Alessandro Virmond <avirmond2000@gmail.com>
2024-10-09 16:01:09 -0300

    Encapsulando propriedades do gamer e tratando argumentos

--- src/main/kotlin/br/com/alura/alugames/principal/TesteGamer.kt
+++ src/main/kotlin/br/com/alura/alugames/principal/TesteGamer.kt
@@ -1,13 +1,21 @@
 import br.com.alura.alugames.modelo.Gamer
 fun main() {
-    val gamer1 = Gamer(nome: "Jaque", email: "jaque@email.com")
+    val gamer1 = Gamer(nome = "Jaque", email = "jaque@email.com")
     println(gamer1)
 
     val gamer2 = Gamer(
-        nome: "Jeni",
-    email: "jeni@email.com",
-    dataNascimento: "19/19/1992",
-        usuario: "jeniblo")
+        nome = "Jeni",
+        email = "jeni@email.com",
+        dataNascimento = "19/19/1992",
+        usuario = "jeniblo"
+    )
 
     println(gamer2)
+
+    gamer1.let {
+        it.dataNascimento = "18/09/2000"
+        it.usuario = "jaqueskywalker"
+    }
+
+    println(gamer1)
 }
